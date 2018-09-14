daa6a5f04d5d4d62278d8ec1ecb5ef51fe6ab34f
kcert2018@gmail.com <kcert2018@gmail.com>
2018-09-14 13:22:27 +0900

    initial project version

---
+++ src/components/Intro.vue
@@ -0,0 +1,213 @@
+<template>
+ <div id="app">
+   <v-app dark>
+    <v-content>
+      <section>
+        <v-parallax src="https://images.wallpaperscraft.com/image/black_background_red_color_paint_explosion_burst_9844_1920x1080.jpg" height="600">
+          <v-layout
+            column
+            align-center
+            justify-center
+            class="white--text"
+          >
+            <img src="http://icons-for-free.com/free-icons/png/1042/190305.png" alt="Vuetify.js" height="250">
+            <br><br>
+            <h1 class="white--text mb-3 display-1 text-xs-center">Movie-Doc</h1>
+            <div class="subheading mb-1 text-xs-center">Product by FA-Linux</div>
+            <v-btn
+              class="red darken-1 mt-3"
+              dark
+              large
+            >
+              Getting Start
+            </v-btn>
+            <v-btn
+              class="red darken-4 mt-2"
+              dark
+              large
+            >
+              Git
+            </v-btn>
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-layout
+          column
+          wrap
+          class="my-5"
+          align-center
+        >
+          <v-flex xs12 sm4 class="my-3">
+            <div class="text-xs-center">
+              <h2 class="headline">Powerful Document Indexing System</h2>
+              <span class="subheading">
+                Vouch Fastest Document Index
+              </span>
+            </div>
+          </v-flex>
+          <v-flex xs12>
+            <v-container grid-list-xl>
+              <v-layout row wrap align-center>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="red--text text--darken-2">accessibility_new</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">More Easier</div>
+                    </v-card-title>
+                    <v-card-text>
+                    Existing document indexing systems would have to spend more time, learn,
+                    and effectively get the information they wanted. But you do not have to
+                    spend your precious time anymore.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="red--text text--darken-2">slideshow</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline">Can Repeat</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Not only can you listen to the desired section over and over again,
+                      but you can also store the necessary parts in your personal space.
+                      You do not have to be on the internet to find the information you once found.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="red--text text--darken-2">find_in_page</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Completely Opened Information</div>
+                    </v-card-title>
+                    <v-card-text>
+                      All information can be viewed without registering to anyone.
+                      Browse the information you need, wherever you want, on any device.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </section>
+
+      <section>
+        <v-parallax src="https://images.wallpaperscraft.com/image/black_background_red_color_paint_explosion_burst_9844_1920x1080.jpg" height="380">
+          <v-layout column align-center justify-center>
+            <div class="headline white--text mb-3 text-xs-center">Index Professional Document has never been easier</div>
+            <em>Easy, and Comportable</em>
+            <v-btn
+              class="red darken-4 mt-3"
+              dark
+              large
+            >
+              Getting Start
+            </v-btn>
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-container grid-list-xl>
+          <v-layout row wrap justify-center class="my-5">
+            <v-flex xs12 sm4>
+              <v-card class="elevation-0 transparent">
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Company info</div>
+                </v-card-title>
+                <v-card-text>
+                  
+The Firmware Lab,
+
+Since its founding in 2005, it has developed 32/64-bit low power processors
+
+Design your embedded and cloud server products
+
+Devoted to research and applied the world's best Linux operating system
+
+All researchers are constantly challenging to secure technology.
+
+I'm trying.
+
+ 
+
+ 
+
+As a result of constant research and challenge
+
+The world's fastest boot solution "ZEROBOOT"
+
+With the achievement of development, the first cloud cluster in Korea
+
+We have developed a low-power server that is the most suitable for the system,
+
+We provide various industrial automation system control technology
+
+We secured it across all areas.
+
+ 
+
+ 
+
+The FLEX lab is implementing the technology sharing spirit.
+
+To solve a number of customer problems
+
+I keep the door open all the time.
+
+Please do not hesitate to contact us.
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 sm4 offset-sm1>
+              <v-card class="elevation-0 transparent">
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Contact us</div>
+                </v-card-title>
+                <v-card-text>
+                  Experience your fastest and comportable document Index system ever.
+                </v-card-text>
+                <v-list class="transparent">
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon class="red--text text--darken-2">phone</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>010-2204-9137</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon class="red--text text--darken-2">place</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>Pangyo Hi-Pex ( A ) 517-4</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon class="red--text text--darken-2">email</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>kjo9148@falinux.com</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </section>
+    </v-content>
+  </v-app>
+ </div>
+</template>
